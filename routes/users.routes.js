30a012aabf2dca3c8bbea466f84364279133b48d
guille garcia <ggarciacardiel@gmail.com>
2023-01-28 19:59:42 +0100

    adds profile routes in users.routes.js

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,4 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/User.model");
+const isLoggedIn = require('../middlewares');
+
+/* GET users listing. */
+router.get('/profile', isLoggedIn, function (req, res, next) {
+  const user = req.session.currentUser;
+  res.render('auth/profile', user);
+});
+
+/* GET users listing. */
+router.get('/profile/edit', isLoggedIn, function (req, res, next) {
+  const user = req.session.currentUser;
+  res.render('auth/profileEdit', user);
+});
+
+/* POST user edit. */
+router.post('/profile/edit', isLoggedIn, async function (req, res, next) {
+  const { username } = req.body;
+  const user = req.session.currentUser;
+  try {
+    const userInDB = await User.findByIdAndUpdate(user._id, { username }, { new: true });
+    req.session.currentUser = userInDB;
+    res.redirect('/profile');
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
