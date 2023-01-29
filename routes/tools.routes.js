57537c57f54ccdf59f1713d48f67d3d0f5fd9a4c
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-01-30 00:57:38 +0100

    post new tool route

--- routes/tools.routes.js
+++ routes/tools.routes.js
@@ -11,4 +11,16 @@ router.get('/tools/new', isLoggedIn, function (req, res, next) {
     res.render('newTool');
   });
 
+  /* POST  GET USERS NEW TOOL */
+/* ROUTE /Tools/new */
+  router.post('/tools/new', isLoggedIn, async function (req, res, next) {
+    const {name, description, image, url,field,tag} = req.body;
+    try {
+      const createdTool = await Tool.create({ name, description, image, url,field,tag });
+      res.redirect(`/`);
+    } catch (error) {
+      next(error)
+    }
+  });
+
 module.exports = router;
