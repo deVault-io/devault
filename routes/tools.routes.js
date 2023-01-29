a11aa55ebeb87808cba984f30690301c85a6a871
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-01-29 23:39:41 +0100

    get view tool route

--- routes/tools.routes.js
+++ routes/tools.routes.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Tool = require("../models/Tool.model");
+const User = require('../models/User.model');
+const isLoggedIn = require('../middlewares');
+
+
+/* GET form view */
+/* ROUTE /Tools/new */
+router.get('/tools/new', isLoggedIn, function (req, res, next) {
+    res.render('newTool');
+  });
 
 module.exports = router;
