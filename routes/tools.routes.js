62825d0c4fccccc248973f08cf726806f556f720
guille garcia <ggarciacardiel@gmail.com>
2023-01-30 20:11:59 +0100

    adds edit tool view and adds edit routes GET/POST

--- routes/tools.routes.js
+++ routes/tools.routes.js
@@ -4,8 +4,6 @@ const User = require("../models/User.model");
 const Tool = require("../models/Tool.model");
 const isLoggedIn = require('../middlewares');
 
-
-
 /* GET form view */
 /* ROUTE /Tools/new 
 USER PROTECTED ROUTE*/
@@ -23,9 +21,9 @@ router.post('/tools/new', isLoggedIn, async function (req, res, next) {
     res.render('newTool', { error: 'image needs to be a valid http:// address'});
     return;
   }
-  const {name, description, image, url,field,tag} = req.body;
+  const {name, description, image, url, field, tag} = req.body;
   try {
-    const createdTool = await Tool.create({ name, description, image, url, field,tag, user: user });
+    const createdTool = await Tool.create({ name, description, image, url, field, tag, user: user });
     res.redirect(`/tools/${createdTool._id}`);
   } catch (error) {
     next(error)
@@ -46,6 +44,35 @@ router.get('/tools/:toolId', async function (req, res, next) {
     const items = await Tool.find({field: tool.field, _id: { $ne: tool._id }}).skip(random).limit(3);
     res.render('toolDetail', { user, tool, items:items  });
     console.log(`random items:${items}`)
+  } catch (error) {
+    next(error)
+  }
+});
+
+/* GET one tool edit */
+/* ROUTE /tools/:toolId/edit */
+router.get('/tools/:toolId/edit', async function (req, res, next) {
+  const { toolId } = req.params;
+  const user = req.session.currentUser;
+  try {
+    const tool = await Tool.findById(toolId).populate('user');
+    res.render('toolEdit', { user, tool });
+  } catch (error) {
+    next(error)
+  }
+});
+
+/* POST Tool Update */
+/* ROUTE tools/:toolId/edit */
+router.post('/tools/:toolId/edit', async (req, res, next) => {
+  const { toolId } = req.params;
+  const user = req.session.currentUser;
+  const { name, description, image, url, field, tag } = req.body;
+  try {
+    const tool = await Tool.findById(toolId).populate('user');
+    const editedTool = await Tool.findByIdAndUpdate(toolId, { name, description, image, url, field, tag, user: user }, {new:true})
+    res.redirect(`/tools/${editedTool._id}`)
+    
   } catch (error) {
     next(error)
   }
