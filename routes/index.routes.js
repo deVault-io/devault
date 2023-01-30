5598397c93b8a2331a697383f40927f5ebde38f8
guille garcia <ggarciacardiel@gmail.com>
2023-01-30 12:23:29 +0100

    adds first version of cards and fixes index routes to display them

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
+const Tool = require("../models/Tool.model");
+const isLoggedIn = require('../middlewares');
 
 // @desc    App home page
 // @route   GET /
 // @access  Public
-router.get('/', (req, res, next) => {
+router.get('/', async function (req, res, next) {
   const user = req.session.currentUser;
-  res.render('index', { title: 'Hello', user });
+  try {
+    const tools = await Tool.find({});
+    res.render('index', { tools });
+    console.log(tools);
+  } catch (error) {
+    next(error)
+  }
 });
 
 module.exports = router;
--- routes/tools.routes.js
+++ routes/tools.routes.js
@@ -8,16 +8,7 @@ const isLoggedIn = require('../middlewares');
 /* ROUTE /tools
 PUBLIC ACCESS */
 
-router.get('/', async function (req, res, next) {
-  // const user = req.session.currentUser;
-  try {
-    const tools = await Tool.find({});
-    res.render('index', { tools });
-    console.log(tools);
-  } catch (error) {
-    next(error)
-  }
-});
+
 
 /* GET form view */
 /* ROUTE /Tools/new 
