647eac1e156056444b0f7703e1a27d46649b0666
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-02-17 21:43:28 +0100

    apply close btns work, css still holding

--- public/javascript/finesearch.dom.js
+++ public/javascript/finesearch.dom.js
@@ -1,22 +1,21 @@
 //enables the toggle button for the advanced filters in the search results pages
 window.addEventListener('load', () => {
-    const searchToggleFilterBtn2 = document.getElementById('search-filter-toggle-btn2');
-    const searchToggleApplyBtn2 = document.getElementById('search-apply-toggle-btn2');
-    const advancedSearchMainContainer = document.getElementsByClassName('advanced-search-main-container');
- 
-  searchToggleFilterBtn2.addEventListener('click', () => {
-    if (advancedSearchMainContainer[0].classList.contains('hidden')) {
-      advancedSearchMainContainer[0].classList.remove('hidden')
-    } else {
-    advancedSearchMainContainer[0].classList.add('hidden');
-    }
-  })
+  const searchToggleFilterBtn = document.getElementById('search-filter-toggle-btn2');
+  const searchToggleCloseBtn = document.getElementById('search-cancel-toggle-btn2');
+  const advancedSearchMainContainer = document.getElementsByClassName('advanced-search-main-container');
 
-  searchToggleApplyBtn2.addEventListener('click', () => {
-    if (advancedSearchMainContainer[0].classList.contains('hidden')) {
-      advancedSearchMainContainer[0].classList.remove('hidden')
-    } else {
-    advancedSearchMainContainer[0].classList.add('hidden');
-    }
-  })
-  })
+  searchToggleFilterBtn.addEventListener('click', () => {
+  if (advancedSearchMainContainer[0].classList.contains('hidden')) {
+    advancedSearchMainContainer[0].classList.remove('hidden')
+  } else {
+  advancedSearchMainContainer[0].classList.add('hidden');
+  }
+})
+searchToggleCloseBtn.addEventListener('click', () => {
+  if (advancedSearchMainContainer[0].classList.contains('hidden')) {
+    advancedSearchMainContainer[0].classList.remove('hidden')
+  } else {
+  advancedSearchMainContainer[0].classList.add('hidden');
+  }
+})
+})
